f51cc4795a2e59de5908638ac309a75228f963ac
Danny <saelid2517@gmail.com>
2020-06-24 19:14:36 -0700

    first commit

---
+++ ipdata.py
@@ -0,0 +1,30 @@
+from version2.models import *
+import ipinfo
+
+access_token = 'b85753035711db'
+handler = ipinfo.getHandler(access_token)
+rpds = Respondent.objects.all()
+
+def country_frequency():
+    cfdict = {}
+    for r in rpds:
+        details = handler.getDetails(r.ip_addr)
+        try:
+            if details.country not in cfdict:
+                cfdict[details.country] = 0
+            cfdict[details.country] += 1
+        except:
+            print("an exception occurred")
+    return cfdict
+
+def city_frequency():
+    cfdict = {}
+    for r in rpds:
+        details = handler.getDetails(r.ip_addr)
+        try:
+            if details.city not in cfdict:
+                cfdict[details.city] = 0
+            cfdict[details.city] += 1
+        except:
+            print("an exception occurred")
+    return cfdict
